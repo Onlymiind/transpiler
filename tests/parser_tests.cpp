b214ba7a0c8b154ab907bebfe878f2bf8d521a5b
onlymind <sasha0darmostuk@gmail.com>
2023-07-06 21:06:00 +0300

    added dirwalk to tests

--- tests/parser_tests.cpp
+++ tests/parser_tests.cpp
@@ -10,22 +10,16 @@
 #include <fstream>
 #include <filesystem>
 
-std::vector<std::string> fnames{
-    "alias_decl", "struct_decl", "func_decl", "expr_in_func",
-    "var_decl", "if_smt", "return_smt", "loops", "complex_types"
-};
-
 std::string base_dir = "../tests/test_data/";
 
 util::StringAllocator alloc;
 
 TEST_CASE("parser test") {
     INFO(std::filesystem::current_path());
-    for(const auto& name : fnames) {
+    for(const auto& entry : std::filesystem::directory_iterator(base_dir)) {
         util::ErrorHandler err;
-        INFO("testing file " + base_dir + name);
-        REQUIRE(std::filesystem::exists(base_dir + name));
-        std::ifstream in{base_dir + name};
+        INFO("testing file " + entry.path().filename().string());
+        std::ifstream in{entry.path()};
         REQUIRE(in.is_open());
 
         REQUIRE_NOTHROW(parser::parse(lexer::Lexer{in, alloc, err}.split(), alloc, err));
