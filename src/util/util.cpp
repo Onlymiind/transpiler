38dd82e6b87a3c0f1834c387d6cf53ed717734c6
onlymind <sasha0darmostuk@gmail.com>
2023-08-07 16:52:14 +0300

    removed old unneded code from util module

--- src/util/util.cpp
+++ src/util/util.cpp
@@ -36,48 +36,6 @@ namespace util {
         return i;
     }
 
-    std::optional<size_t> consume_scopes(types::Tokens tokens, size_t start, std::unordered_map<types::Category, types::Category> scope_delimiters) {
-        if(start >= tokens.size() || !scope_delimiters.contains(tokens[start].category)) {
-            return start;
-        }
-
-        std::stack<types::Category> scope;
-        auto inverse = util::inverse(scope_delimiters);
-        size_t i = start;
-        for(; i < tokens.size(); i++) {
-            if(scope_delimiters.contains(tokens[i].category)) {
-                scope.push(tokens[i].category);
-            } else if(inverse.contains(tokens[i].category)) {
-                if(scope.top() != inverse[tokens[i].category]) {
-                    return {};
-                }
-                scope.pop();
-            }
-
-
-            if(scope.empty()) {
-                break;
-            }
-        }
-
-        return i;
-    }
-
-    types::Tokens split(types::Tokens& tokens, types::Category delim) {
-        if(tokens.empty()) {
-            return tokens;
-        }
-
-        auto offset = find_in_current_scope(tokens, delim);
-        if(!offset) {
-            return types::Tokens{};
-        }
-
-        auto result = tokens.subspan(0, *offset);
-        tokens = tokens.subspan(std::min(tokens.size(), *offset + 1));
-        return result;
-    }
-
     std::optional<size_t> find_in_current_scope(types::Tokens tokens, types::Category cat) {
         size_t scope{};
         for(size_t i = 0; i < tokens.size(); i++) {
@@ -94,31 +52,6 @@ namespace util {
 
             if(scope == 0 && tokens[i].category == cat) {
                 return i;
-            }
-        }
-
-        return {};
-    }
-
-    std::optional<std::pair<types::Category, size_t>> find_in_current_scope(types::Tokens tokens, const std::unordered_set<types::Category>& categories) {
-        size_t scope{};
-        bool has_lbrace = categories.contains(types::Category::LBRACE);
-        for(size_t i = 0; i < tokens.size(); i++) {
-
-            types::Category cat = tokens[i].category;
-            
-            // "{" will be consumed by util::consume_scope, so check for it here 
-            if(has_lbrace && cat == types::Category::LBRACE) {
-                return std::pair{cat, i};
-            }
-
-            i = util::consume_scope(tokens, i, {types::Category::LBRACE, types::Category::RBRACE});
-            if(i >= tokens.size()) {
-                break;
-            }
-
-            if(scope == 0 && categories.contains(cat)) {
-                return std::pair{cat, i};
             }
         }
 
--- src/util/util.h
+++ src/util/util.h
@@ -18,36 +18,10 @@
 #include "types/token.h"
 
 namespace util {
-    struct Error {
-        size_t pos = 0;
-        std::string msg;
-    };
-
-    inline std::ostream& operator<<(std::ostream& out, const Error& err) {
-        out << "Error on pos " << err.pos << ", message: " << err.msg;
-        return out;
-    }
-
-    template<typename Key, typename Val>
-    inline std::unordered_map<Val, Key> inverse(const std::unordered_map<Key, Val>& map) {
-        std::unordered_map<Val, Key> inverse;
-        inverse.reserve(map.size());
-        std::transform(map.begin(), map.end(), std::inserter(inverse, inverse.begin()), [](const auto& pair) {
-            return std::pair<Val, Key>{pair.second, pair.first};
-        });
-
-        return inverse;
-    }
 
     size_t consume_scope(types::Tokens tokens, size_t start, std::pair<types::Category, types::Category> scope_delimiters);
 
-    std::optional<size_t> consume_scopes(types::Tokens tokens, size_t start, std::unordered_map<types::Category, types::Category> scope_delimiters);
-
-    types::Tokens split(types::Tokens& tokens, types::Category delim);
-
     std::optional<size_t> find_in_current_scope(types::Tokens tokens, types::Category cat);
-
-    std::optional<std::pair<types::Category, size_t>> find_in_current_scope(types::Tokens tokens, const std::unordered_set<types::Category>& categories);
 
     template<typename T, typename... Args>
     concept Function = std::is_invocable_v<T, Args...>;
